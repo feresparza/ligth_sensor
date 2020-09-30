68c5f57614755df873f73d790392b5a892892688
feresparza <70345011+feresparza@users.noreply.github.com>
2020-09-30 15:01:12 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+let light2 = 0
+basic.forever(function () {
+    light2 = input.lightLevel()
+    if (light2 >= 0 && light2 <= 85) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (light2 >= 86 && light2 <= 179) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
